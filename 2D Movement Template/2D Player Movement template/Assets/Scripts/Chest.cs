b224509ad29653b5f071441a80ae3562e147fb48
3phraimY <ephraim.young@cuw.edu>
2024-02-20 19:55:48 -0600

    coins and chest animations

---
+++ 2D Movement Template/2D Player Movement template/Assets/Scripts/Chest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public int coinsRequired;
+    public Animator chest;
+    public Sprite OpenChest;
+    private bool hasRun = false;
+
+    IEnumerator slowDown()
+    {
+        
+        yield return new WaitForSeconds(.5f);
+        Destroy(chest);
+        this.GetComponent<SpriteRenderer>().sprite = OpenChest;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Singleton.Score >= coinsRequired && !hasRun)
+        {
+            hasRun = true; 
+            chest.SetBool("Coins", true);
+            StartCoroutine(slowDown());            
+        }
+    }
+}
--- 2D Movement Template/2D Player Movement template/Assets/Scripts/Singleton.cs
+++ 2D Movement Template/2D Player Movement template/Assets/Scripts/Singleton.cs
@@ -3,20 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 
-public class Singleton : MonoBehaviour
-{
-    public static Singleton singleton;
+public class Singleton
+{ 
     public static int Score;
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (singleton == null)
-            singleton = this;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
